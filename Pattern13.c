c27ddc3b3129d61c1a2d2b922a9db2354e64d7b6
SHIHAB KHAN <41753677+shihab0005@users.noreply.github.com>
2020-02-14 04:44:52 -0800

    Add files via upload

---
+++ Pattern13.c
@@ -0,0 +1,38 @@
+/*
+input:
+Enter Range =5
+output:
+A
+A B
+A B C
+A B C D
+A B C D E
+A B C D
+A B C
+A B
+A
+*/
+
+
+#include<stdio.h>
+int main()
+{
+	int row,col,n;
+	
+	printf("Enter Range :");
+	scanf("%d",&n);
+	
+	for(row=1;row<=n;row++){
+		for(col=1;col<=row;col++){
+			printf("%c ", col+64);
+		}
+		printf("\n");
+	}
+	for(row=n-1;row>=1;row--){
+		for(col=1;col<=row;col++){
+			printf("%c ", col+64);
+		}
+		printf("\n");
+	}
+	
+}
